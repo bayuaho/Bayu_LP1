80b7d363295f23950aac12ef7959dafb2b484ea5
bayuaho <bayualamsyah5127@gmail.com>
2026-05-09 16:15:52 +0800

    tambahan model item dan categories

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Endpoint test sederhana
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('items', ItemController::class);
+Route::get('/test', function () {
+    
+    return response()->json([
+        'status' => 'success',
+        'message' => 'API endpoint berhasil diakses',
+        'data' => [
+            'nama' => 'Bayu Alamsyah Pabarani',
+            'nim' => '60200124099',
+            'matkul' => 'Environment dan Repository'
+        ]
+    ], 200);
+});
